2932aae2bb8efe547ecda500726d0939dc887e6e
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-10-28 01:00:42 -0500

    Revert "Revert "Lesson 8"" Lesson 8 This reverts commit d143c1f73b51b30b791eb7658c92b4d9e40181d1.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-/*
 // Get the container element //
 const container = document.querySelector('#root');
 
 // Initialize the root from the container //
 const root = ReactDOM.createRoot(container);
 
-// Intialize Custom Component //
-function MainContent() {
-  return <h1>I'm Learning React!</h1>;
-}
+// Create a navbar //
+const navbar = (
+  <nav>
+    <h1>Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
 
-// Render some output //
-root.render(<h1>Hello, React!</h1>);
-*/
-
-// Recreate with Vanilla JS
-const headingOne = document.createElement('h1');
-headingOne.textContent = 'Hello, React!';
-headingOne.className = 'header';
-document.querySelector('#root').appendChild(headingOne);
+// Render the navbar //
+root.render(navbar);
