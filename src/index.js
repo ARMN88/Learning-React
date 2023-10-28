8f6e1c96d269afb5eb250a6f3763898da3a09e3f
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-10-28 00:20:58 -0500

    Lesson 6

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ const container = document.querySelector('#root');
 // Initialize the root from the container //
 const root = ReactDOM.createRoot(container);
 
+// Intialize Custom Component //
+function MainContent() {
+  return <h1>I'm Learning React!</h1>;
+}
+
 // Render some output //
-root.render(<h1>Hello, everyone!</h1>);
+root.render(
+  <div>
+    <MainContent />
+  </div>
+);
